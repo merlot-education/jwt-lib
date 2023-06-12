dda41000528a671ec14712b4858dec4ac532a863
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-06-12 13:00:04 +0200

    added initial jwt lib

---
+++ src/main/java/eu/merloteducation/jwtlib/config/AutoConfigure.java
@@ -0,0 +1,10 @@
+package eu.merloteducation.jwtlib.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan
+public class AutoConfigure {
+    // intentionally left empty
+}
