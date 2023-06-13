f81cda096c27e64fd7dbc8225616bb95f01c4ac9
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-06-13 10:37:56 +0200

    added component to config

--- src/main/java/eu/merloteducation/jwtlib/config/AutoConfigure.java
+++ src/main/java/eu/merloteducation/jwtlib/config/AutoConfigure.java
@@ -1,10 +1,18 @@
 package eu.merloteducation.jwtlib.config;
 
+import eu.merloteducation.jwtlib.JwtAuthConverter;
+import eu.merloteducation.jwtlib.JwtAuthConverterProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @AutoConfiguration
 @ComponentScan
 public class AutoConfigure {
-    // intentionally left empty
+    @Bean
+    @ConditionalOnMissingBean
+    public JwtAuthConverter jwtAuthConverter() {
+        return new JwtAuthConverter(new JwtAuthConverterProperties());
+    }
 }
